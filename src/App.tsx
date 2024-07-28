471902592007a0bf316548055c110fbafd01c15a
EsatAdiloglu <esatadiloglu16@gmail.com>
2024-07-27 21:35:04 -0400

    version 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-const getFinalsData = async (e:any) => {
+const getFinalsData = async () => {
   try {
-    e.preventDefault();
     const response = await fetch("https://localhost:3000/api/finals/BIA 500/A");
     const data = await response.json();
     console.log(data);
@@ -39,6 +38,10 @@ function App() {
     sectionInput: "",
   });
   const [outputData, setOutputData] = useState<React.ReactNode>(null)
+
+  useEffect(() => {
+    getFinalsData();
+  })
 
   const handleChange = (e: any) => {
     const {name, value} = e.target;
