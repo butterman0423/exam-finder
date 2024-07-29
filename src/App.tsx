83c2a757e0b26c3b9a785fd658d9809068ff8fb2
EsatAdiloglu <esatadiloglu16@gmail.com>
2024-07-29 18:39:12 -0400

    :)

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import { ReactComponent as StevensDucksLogo } from './Stevens_Ducks.svg';
 
-// const fetchCourseFinals = async () => {
-//   try{
-//     const response = await fetch();
-//     const data = response.json();
-//   }
-//   catch(error){
-//     alert("Whoops, something went wrong. Please try again.");
-//     console.log(error);
-//   }
-// }
 interface OutputFinalProps {
   data: string
-}
+};
+
 interface GetFinalProps {
   classname: string,
   section: string
+};
+
+const getFinalsData = async () => {
+  try{
+    const response = await fetch("https://localhost:3000/api/finals/BIA 500/A");
+    const data = await response.json();
+    console.log(data);
+  }
+  catch(error){
+    console.log(error);
+  }
 }
 
 const OutputFinal: React.FC<OutputFinalProps> = ({data}) => {
@@ -36,7 +38,13 @@ function App() {
     classInput: "",
     sectionInput: "",
   });
-  const [outputData, setOutputData] = useState<React.ReactNode>(null)
+  const [outputData, setOutputData] = useState<{ [key: string]: string }>({})
+
+  /*
+  useEffect(() => {
+    getFinalsData();
+  })
+  */
 
   const handleChange = (e: any) => {
     const {name, value} = e.target;
@@ -45,9 +53,24 @@ function App() {
       [name]: value,
     });
   };
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    setOutputData(<GetFinal classname={finalData.classInput} section={finalData.sectionInput} />)
+    const { classInput, sectionInput } = finalData;
+    
+    try {
+        const dat = await fetch(`/api/finals/${classInput}/${sectionInput}`);
+        const json = await dat.json();
+
+        console.log(json);
+        if(json === undefined) {
+          throw Error(`${classInput} ${sectionInput} does not exist`)
+        }
+
+        setOutputData(json[0]);
+        //setOutputData(<GetFinal classname={ json['day_date'] } section={ json['building_room'] } />)
+    } catch(e) {
+      console.error(e);
+    }
   }
   return (
     <div className="App">
@@ -76,7 +99,14 @@ function App() {
         </form>
       </div>
       <div id="outputSection">
-        {outputData}
+        {
+          outputData && Object.keys(outputData).length > 0 ? 
+          <div>
+            <h1>{outputData['day_date']}, {outputData['building_room']}</h1>
+            <h1>{outputData['start_time']}, {outputData['end_time']}</h1>
+          </div> :
+          ""
+        }
       </div>
     </div>
   );
